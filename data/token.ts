4aca167bef013112949c72b33bdc0b99692f104e
FjrREPO <jossfajar21@gmail.com>
2025-03-16 02:50:57 +0700

    Add initial project setup with environment configuration, Vercel deployment settings, and API routes for tokens and tweets

---
+++ data/token.ts
@@ -0,0 +1,92 @@
+export const TOKEN_DATA = [
+  {
+    "name": "USD Coin",
+    "symbol": "USDC",
+    "address": "0xdC5365fa140b187408f730B09C9E4eedEd821410",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+    "price": 1,
+    "tags": ["NATIVE TOKEN"]
+  },
+  {
+    "name": "Ethereum",
+    "symbol": "ETH",
+    "address": "0xb9d8963158292beE31aF6F5adb86923699844EC5",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png",
+    "price": 2719,
+    "tags": ["TOP TIER", "TOP 10 MARKET"]
+  },
+  {
+    "name": "Bitcoin",
+    "symbol": "BTC",
+    "address": "0xdDA5Ce3266a4D996df2Cc0f8B8F5a1265CB08dbA",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
+    "price": 65300,
+    "tags": ["TOP TIER", "TOP 10 MARKET"]
+  },
+  {
+    "name": "Wrapped Ether",
+    "symbol": "WETH",
+    "address": "0xaf7c25364340FFE723D1cbb5F97B0a85371D629d",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png",
+    "price": 2719,
+    "tags": ["WRAPPED TOKEN"]
+  },
+  {
+    "name": "PEPE",
+    "symbol": "PEPE",
+    "address": "0xdaaF857E8CE014A6297fe9EF7773A07C8c3f7249",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://dd.dexscreener.com/ds-data/tokens/solana/B5WTLaRwaUQpKk7ir1wniNB6m5o8GgMrimhKMYan2R6B.png",
+    "price": 0.0011,
+    "tags": ["TOP 10 MARKET"]
+  },
+  {
+    "name": "OFFICIAL TRUMP",
+    "symbol": "TRUMP",
+    "address": "0x416228aC9aA0c4fbe25B85F45cb27833A92aF2a6",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/35336.png",
+    "price": 15.92,
+    "tags": ["TOP 10 MARKET"]
+  },
+  {
+    "name": "DOGE AI",
+    "symbol": "DOGEAI",
+    "address": "0x059b916F86793376c3805830A56939d26712cCa5",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/35601.png",
+    "price": 0.062,
+    "tags": ["TOP 10 MARKET"]
+  },
+  {
+    "name": "dogwifhat",
+    "symbol": "WIF",
+    "address": "0x7873537380B3C5978629F8fF618a0451B4bf6f7D",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/64x64/28752.png",
+    "price": 0.77,
+    "tags": []
+  },
+  {
+    "name": "STONKS",
+    "symbol": "STONKS",
+    "address": "0xd8e76e3D5367688131d716a3156A40ddE55DAeA2",
+    "chain": "Base Sepolia",
+    "decimals": 18,
+    "logoURI": "https://s2.coinmarketcap.com/static/img/coins/200x200/29560.png",
+    "price": 0.05,
+    "tags": []
+  }
+];
